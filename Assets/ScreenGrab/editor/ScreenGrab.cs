700a318b4c74bb8f82b67168fdc228340681e3d8
Justin Bakse <justin@tsfim.com>
2018-03-22 15:18:44 -0400

    initial commit

---
+++ Assets/ScreenGrab/editor/ScreenGrab.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ScreenGrab : MonoBehaviour {
+
+	[MenuItem("ScreenGrab/Save Screengrab")]
+	static void interactiveScreenGrab() {
+		Debug.Log("Screen Grab");
+		Debug.Log(Camera.main);
+        Camera cam = Camera.main;
+        
+		// // create render
+		RenderTexture render = RenderTexture.GetTemporary(1920, 1080, 24, RenderTextureFormat.DefaultHDR, RenderTextureReadWrite.Default, 1);
+        RenderTexture tempTargetTexture = cam.targetTexture;
+		cam.targetTexture = render;
+		cam.Render();
+		
+
+		// // read render texture pixels
+		RenderTexture tempActiveRenderTexture = RenderTexture.active;
+        RenderTexture.active = render;
+        Texture2D tex = new Texture2D(render.width, render.height);
+		// revist: public Texture2D(int width, int height, TextureFormat format, bool mipmap, bool linear);
+        tex.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
+		tex.Apply();
+
+		// // encode as png, save out
+        var bytes = tex.EncodeToPNG();
+        System.IO.File.WriteAllBytes("render.png", bytes);
+        
+
+		Texture2D texHDR = new Texture2D(render.width, render.height, TextureFormat.RGBAHalf, false);
+		// revist: public Texture2D(int width, int height, TextureFormat format, bool mipmap, bool linear);
+        texHDR.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
+		texHDR.Apply();
+
+		Color[] colors = texHDR.GetPixels(0);
+		Debug.Log(colors[0]);
+        MiniEXR.MiniEXR.MiniEXRWriteRGBA("render.exr", 1920, 1080, colors);
+        
+		// // clean up
+		RenderTexture.active = tempActiveRenderTexture;
+		RenderTexture.ReleaseTemporary(render);
+		cam.targetTexture = tempTargetTexture;
+		cam.ResetAspect();
+
+	}
+// related http://williamchyr.com/2013/11/unity-shaders-depth-and-normal-textures/
+
+// related http://answers.unity3d.com/questions/9969/convert-a-rendertexture-to-a-texture2d.html
+
+// 	public static RenderTexture Snapshot(int width, int height) {
+
+//        var result = RenderTexture.GetTemporary(width, height, 24);
+
+//        if (CaptureTheGIF.captureCamera != null)
+//        {
+//            Camera cam = CaptureTheGIF.captureCamera;
+//            var old = cam.targetTexture;
+//            cam.targetTexture = result;
+//            cam.Render();
+//            cam.targetTexture = old;
+//            cam.ResetAspect();
+
+//            return result;
+//        }
+       
+//        foreach(var cam in Camera.allCameras.OrderBy(c => c.depth)) {
+
+//            var old = cam.targetTexture;
+//            cam.targetTexture = result;
+//            cam.Render();
+//            cam.targetTexture = old;
+//            cam.ResetAspect();
+//        }
+//        return result;
+//    }
+
+
+}
---
+++ Assets/cyclorama/CameraRecorder.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRecorder : MonoBehaviour
+{
+    private int real_frame;
+    public int start_frame = 1;
+    public int end_frame = 60;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update() {
+        if (Input.GetKeyDown("space")){
+            Capture();
+        }
+
+    }
+
+    void Capture()
+    {
+        Debug.Log("Capture");
+        ScreenCapture.CaptureScreenshot("render/render.png");
+    }
+
+
+    // Update is called once per frame
+    // void OnPostRender()
+    // {
+    //     real_frame++;
+    //     int frameSkip = 2;
+    //     if (real_frame % frameSkip > 0)
+    //     {
+    //         return;
+    //     }
+
+    //     string name_prefix = "render/render";
+    //     int frame = real_frame / frameSkip;
+    //     string screenshot_name = name_prefix + "_" + frame.ToString("000000") + ".png";
+
+    //     if (frame > start_frame && frame < end_frame)
+    //     {
+    //         // Application.CaptureScreenshot(screenshot_name);
+    //         Debug.Log("Render frame: " + frame);
+
+    //         Texture2D tex = new Texture2D(Screen.width, Screen.height);
+    //         tex.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+    //         tex.Apply();
+
+    //     	// var bytes = tex.EncodeToPNG();
+    //     	// System.IO.File.WriteAllBytes(screenshot_name, bytes);
+    //     	// UnityEngine.Object.Destroy(tex);
+
+
+    //     }
+    // }
+
+    // public static void SaveTextureToFile(RenderTexture renderTexture, string name)
+    // {
+    //     RenderTexture currentActiveRT = RenderTexture.active;
+    //     RenderTexture.active = renderTexture;
+    //     Texture2D tex = new Texture2D(renderTexture.width, renderTexture.height);
+    //     tex.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
+    //     var bytes = tex.EncodeToPNG();
+    //     System.IO.File.WriteAllBytes(name, bytes);
+    //     UnityEngine.Object.Destroy(tex);
+    //     RenderTexture.active = currentActiveRT;
+    // }
+}
